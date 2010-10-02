a954b1b8a0adee7645d60398967f4fcc0a5f3bbf
Kitty Barnett <develop@catznip.com>
2010-10-02 11:37:25 +0200

    - review : reviewed for SL-2.2.0
    --HG--
    branch : Inventory-AttachmentEdit

--- indra/newview/llpanelwearing.cpp
+++ indra/newview/llpanelwearing.cpp
@@ -42,7 +42,7 @@ static void edit_outfit()
 	LLSideTray::getInstance()->showPanel("sidepanel_appearance", LLSD().with("type", "edit_outfit"));
 }
 
-// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.1.2a) | Added: Catznip-2.1.2a
+// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.2.0a) | Added: Catznip-2.1.2a
 static void edit_item(const LLUUID& idItem)
 {
 	const LLViewerInventoryItem* pItem = gInventory.getItem(idItem);
@@ -125,13 +125,13 @@ protected:
 		functor_t take_off = boost::bind(&LLAppearanceMgr::removeItemFromAvatar, LLAppearanceMgr::getInstance(), _1);
 
 //		registrar.add("Wearing.Edit", boost::bind(&edit_outfit));
-// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.1.2a) | Added: Catznip-2.1.2a
+// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.2.0a) | Added: Catznip-2.1.2a
 		registrar.add("Wearing.EditItem", boost::bind(handleMultiple, edit_item, mUUIDs));
 		registrar.add("Wearing.EditOutfit", boost::bind(&edit_outfit));
 // [/SL:KB]
 		registrar.add("Wearing.TakeOff", boost::bind(handleMultiple, take_off, mUUIDs));
 		registrar.add("Wearing.Detach", boost::bind(handleMultiple, take_off, mUUIDs));
-// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.1.2a) | Added: Catznip-2.1.2a
+// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.2.0a) | Added: Catznip-2.1.2a
 		registrar.add("Wearing.TakeOffDetach", boost::bind(handleMultiple, take_off, mUUIDs));
 // [/SL:KB]
 
@@ -178,13 +178,13 @@ protected:
 		bool allow_detach = !bp_selected && !clothes_selected && attachments_selected;
 		bool allow_take_off = !bp_selected && clothes_selected && !attachments_selected;
 
-// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.1.2a) | Added: Catznip-2.1.2a
+// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.2.0a) | Added: Catznip-2.1.2a
 		menu->setItemVisible("edit_item",	bp_selected || clothes_selected || attachments_selected);
 		menu->setItemEnabled("edit_item",	1 == mUUIDs.size());
 // [/SL:KB]
 		menu->setItemVisible("take_off",	allow_take_off);
 		menu->setItemVisible("detach",		allow_detach);
-// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.1.2a) | Added: Catznip-2.1.2a
+// [SL:KB] - Patch: Inventory-AttachmentEdit - Checked: 2010-09-04 (Catznip-2.2.0a) | Added: Catznip-2.1.2a
 		menu->setItemVisible("take_off_or_detach", (!allow_detach) && (!allow_take_off) && (clothes_selected) && (attachments_selected));
 // [/SL:KB]
 		menu->setItemVisible("edit_outfit_separator", allow_take_off || allow_detach);
