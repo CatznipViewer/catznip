a00da5630e031ac2ef5d076f19a91fdf9124dd5b
Kitty Barnett <develop@catznip.com>
2011-10-30 22:19:36 +0100

    - internal : reviewed for SL-3.2
    --HG--
    branch : Chat-Misc

--- indra/newview/llchiclet.cpp
+++ indra/newview/llchiclet.cpp
@@ -750,7 +750,7 @@ void LLIMP2PChiclet::updateMenuItems()
 	bool is_friend = LLAvatarActions::isFriend(getOtherParticipantId());
 	mPopupMenu->getChild<LLUICtrl>("Add Friend")->setEnabled(!is_friend);
 
-// [SL:KB] - Patch: Chat-ChicletContextMenu | Checked: 2010-12-01 (Catznip-2.6.0a) | Added: Catznip-2.4.0g
+// [SL:KB] - Patch: Chat-ChicletContextMenu | Checked: 2010-12-01 (Catznip-3.2.0a) | Added: Catznip-2.4.0g
 	bool can_teleport = LLAvatarActions::canOfferTeleport(getOtherParticipantId());
 	mPopupMenu->getChild<LLUICtrl>("Teleport")->setEnabled(can_teleport);
 // [/SL:KB]
@@ -785,7 +785,7 @@ void LLIMP2PChiclet::onMenuItemClicked(const LLSD& user_data)
 	{
 		LLAvatarActions::requestFriendshipDialog(other_participant_id);
 	}
-// [SL:KB] - Patch: Chat-ChicletContextMenu | Checked: 2010-12-01 (Catznip-2.6.0a) | Added: Catznip-2.4.0g
+// [SL:KB] - Patch: Chat-ChicletContextMenu | Checked: 2010-12-01 (Catznip-3.2.0a) | Added: Catznip-2.4.0g
 	else if ("teleport" == level)
 	{
 		LLAvatarActions::offerTeleport(other_participant_id);
