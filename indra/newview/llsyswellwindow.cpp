8f5b5c49efb55a6e09e92941f3a0bf2e26a3ef7e
Kitty Barnett <develop@catznip.com>
2012-02-26 00:24:29 +0100

    - fixed : persistent notifications in the syswell aren't sorted by date
    --HG--
    branch : UI-Notifications

--- indra/newview/llsyswellwindow.cpp
+++ indra/newview/llsyswellwindow.cpp
@@ -442,6 +442,30 @@ BOOL LLIMWellWindow::ObjectRowPanel::handleRightMouseDown(S32 x, S32 y, MASK mas
 /*         LLNotificationWellWindow implementation                      */
 /************************************************************************/
 
+// [SL:KB] - Patch: Notification-Misc | Checked: 2012-02-26 (Catznip-3.2.3) | Added: Catznip-3.2.3
+class LLNotificationDateComparator : public LLFlatListView::ItemComparator
+{
+public:
+	LLNotificationDateComparator() {}
+	/*virtual*/ ~LLNotificationDateComparator() {}
+
+	/*virtual*/ bool compare(const LLPanel* pLHS, const LLPanel* pRHS) const
+	{
+		const LLSysWellItem* pItemLeft = dynamic_cast<const LLSysWellItem*>(pLHS);
+		const LLSysWellItem* pItemRight= dynamic_cast<const LLSysWellItem*>(pRHS);
+		if ( (pItemLeft) && (pItemRight) )
+		{
+			LLNotificationPtr notifLeft = LLNotifications::instance().find(pItemLeft->getID());
+			LLNotificationPtr notifRight= LLNotifications::instance().find(pItemRight->getID());
+			// NOTE: we want to sort notifications from old to new
+			return (notifLeft.get()) && (notifRight.get()) && (notifLeft.get()->getDate() > notifRight.get()->getDate());
+		}
+		return false;
+	}
+};
+static const LLNotificationDateComparator NOTIF_DATE_COMPARATOR;
+// [/SL:KB]
+
 //////////////////////////////////////////////////////////////////////////
 // PUBLIC METHODS
 LLNotificationWellWindow::LLNotificationWellWindow(const LLSD& key)
@@ -463,6 +487,9 @@ LLNotificationWellWindow* LLNotificationWellWindow::getInstance(const LLSD& key 
 BOOL LLNotificationWellWindow::postBuild()
 {
 	BOOL rv = LLSysWellWindow::postBuild();
+// [SL:KB] - Patch: Notification-Misc | Checked: 2012-02-26 (Catznip-3.2.3) | Added: Catznip-3.2.3
+	mMessageList->setComparator(&NOTIF_DATE_COMPARATOR);
+// [/SL:KB]
 	setTitle(getString("title_notification_well_window"));
 	return rv;
 }
@@ -488,8 +515,15 @@ void LLNotificationWellWindow::addItem(LLSysWellItem::Params p)
 		return;
 
 	LLSysWellItem* new_item = new LLSysWellItem(p);
-	if (mMessageList->addItem(new_item, value, ADD_TOP))
+//	if (mMessageList->addItem(new_item, value, ADD_TOP))
+// [SL:KB] - Patch: Notification-Misc | Checked: 2012-02-26 (Catznip-3.2.3) | Added: Catznip-3.2.3
+	if (mMessageList->addItem(new_item, value, ADD_TOP, false))
+// [/SL:KB]
 	{
+// [SL:KB] - Patch: Notification-Misc | Checked: 2012-02-26 (Catznip-3.2.3) | Added: Catznip-3.2.3
+		mMessageList->sort();
+// [/SL:KB]
+
 		mSysWellChiclet->updateWidget(isWindowEmpty());
 		reshapeWindow();
 
