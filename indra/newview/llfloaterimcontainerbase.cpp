c8fed2f1cc24f1cca0f0c57d01895bc0acf1b083
Kitty Barnett <develop@catznip.com>
2013-08-20 21:30:51 +0200

    - internal : rearranged functions in llfloaterimcontainerbase.cpp into a more logical order     -> cut/paste operations only, no actual code changes
    --HG--
    branch : Chat-Base

--- indra/newview/llfloaterimcontainerbase.cpp
+++ indra/newview/llfloaterimcontainerbase.cpp
@@ -49,6 +49,81 @@ LLFloaterIMContainerBase::~LLFloaterIMContainerBase()
 	LLTransientFloaterMgr::getInstance()->removeControlView(LLTransientFloaterMgr::IM, this);
 }
 
+BOOL LLFloaterIMContainerBase::postBuild()
+{
+	// Do not call base postBuild to not connect to mCloseSignal to not close all floaters via Close button
+	// mTabContainer will be initialized in LLMultiFloater::addChild()
+	setTabContainer(getChild<LLTabContainer>("im_box_tab_container"));
+
+	return TRUE;
+}
+
+void LLFloaterIMContainerBase::setMinimized(BOOL b)
+{
+	bool was_minimized = isMinimized();
+	LLMultiFloater::setMinimized(b);
+
+	// Switching from minimized to un-minimized
+	if (was_minimized && !b)
+	{
+		const LLUUID& session_id = getSelectedSession();
+		LLFloaterIMSessionTab* session_floater = LLFloaterIMSessionTab::findConversation(session_id);
+
+		if (session_floater && !session_floater->isTornOff())
+		{
+			// When in DND mode, remove stored IM notifications
+			// Nearby chat (Null) IMs are not stored while in DND mode, so can ignore removal
+			if (gAgent.isDoNotDisturb() && session_id.notNull())
+			{
+				LLDoNotDisturbNotificationStorage::getInstance()->removeNotification(LLDoNotDisturbNotificationStorage::toastName, session_id);
+			}
+		}
+	}
+}
+
+// virtual
+void LLFloaterIMContainerBase::addFloater(LLFloater* floaterp, BOOL select_added_floater, LLTabContainer::eInsertionPoint insertion_point)
+{
+	if (!floaterp)
+		return;
+
+	// already here
+	if (floaterp->getHost() == this)
+	{
+		openFloater(floaterp->getKey());
+		return;
+	}
+
+	LLUUID session_id = floaterp->getKey();
+
+	// Add the floater
+	LLMultiFloater::addFloater(floaterp, select_added_floater, insertion_point);
+
+	LLIconCtrl* icon = 0;
+	if (gAgent.isInGroup(session_id, TRUE))
+	{
+		LLGroupIconCtrl::Params icon_params;
+		icon_params.group_id = session_id;
+		icon = LLUICtrlFactory::instance().create<LLGroupIconCtrl>(icon_params);
+	}
+	else
+	{
+		LLAvatarIconCtrl::Params icon_params;
+		icon_params.avatar_id = session_id.notNull() ? LLIMModel::getInstance()->getOtherParticipantID(session_id) : LLUUID();
+		icon = LLUICtrlFactory::instance().create<LLAvatarIconCtrl>(icon_params);
+	}
+	mTabContainer->setTabImage(floaterp, icon);
+
+	mSessions[session_id] = floaterp;
+	floaterp->mCloseSignal.connect(boost::bind(&LLFloaterIMContainerBase::onCloseFloater, this, session_id));
+}
+
+// static
+bool LLFloaterIMContainerBase::isConversationLoggingAllowed()
+{
+	return gSavedPerAccountSettings.getS32("KeepConversationLogTranscripts") > 0;
+}
+
 // static
 void LLFloaterIMContainerBase::onCurrentChannelChanged(const LLUUID& session_id)
 {
@@ -56,6 +131,46 @@ void LLFloaterIMContainerBase::onCurrentChannelChanged(const LLUUID& session_id)
 	{
 		LLFloaterIMContainerBase::getInstance()->showConversation(session_id);
 	}
+}
+
+void LLFloaterIMContainerBase::onCloseFloater(const LLUUID& session_id)
+{
+	mSessions.erase(session_id);
+	setFocus(TRUE);
+}
+
+// static
+LLFloaterIMContainerBase* LLFloaterIMContainerBase::findInstance()
+{
+	return LLFloaterReg::findTypedInstance<LLFloaterIMContainerBase>("im_container");
+}
+
+// static
+LLFloaterIMContainerBase* LLFloaterIMContainerBase::getInstance()
+{
+	return LLFloaterReg::getTypedInstance<LLFloaterIMContainerBase>("im_container");
+}
+
+// static
+LLFloater* LLFloaterIMContainerBase::buildFloater(const LLSD& sdKey)
+{
+	LLFloater* pIMContainer = NULL;
+	if (gSavedSettings.getBOOL("IMUseTabbedContainer"))
+		pIMContainer = new LLFloaterIMContainerTab(sdKey);
+	else
+		pIMContainer = new LLFloaterIMContainerView(sdKey);
+	return pIMContainer;
+}
+
+// static
+const std::string& LLFloaterIMContainerBase::getFloaterXMLFile()
+{
+	static std::string strFile;
+	strFile = 
+		(gSavedSettings.getBOOL("IMUseTabbedContainer")) 
+			? (!gSavedSettings.getBOOL("IMUseVerticalTabs")) ? "floater_im_container_tab_horiz.xml" : "floater_im_container_tab_vert.xml"
+			: "floater_im_container.xml";
+	return strFile;
 }
 
 // static
@@ -118,121 +233,6 @@ void LLFloaterIMContainerBase::onToggleTabbedContainer()
 // static
 void LLFloaterIMContainerBase::onToggleVerticalTabs()
 {
-}
-
-BOOL LLFloaterIMContainerBase::postBuild()
-{
-	// Do not call base postBuild to not connect to mCloseSignal to not close all floaters via Close button
-	// mTabContainer will be initialized in LLMultiFloater::addChild()
-	setTabContainer(getChild<LLTabContainer>("im_box_tab_container"));
-
-	return TRUE;
-}
-
-// virtual
-void LLFloaterIMContainerBase::addFloater(LLFloater* floaterp, BOOL select_added_floater, LLTabContainer::eInsertionPoint insertion_point)
-{
-	if (!floaterp)
-		return;
-
-	// already here
-	if (floaterp->getHost() == this)
-	{
-		openFloater(floaterp->getKey());
-		return;
-	}
-
-	LLUUID session_id = floaterp->getKey();
-
-	// Add the floater
-	LLMultiFloater::addFloater(floaterp, select_added_floater, insertion_point);
-
-	LLIconCtrl* icon = 0;
-	if (gAgent.isInGroup(session_id, TRUE))
-	{
-		LLGroupIconCtrl::Params icon_params;
-		icon_params.group_id = session_id;
-		icon = LLUICtrlFactory::instance().create<LLGroupIconCtrl>(icon_params);
-	}
-	else
-	{
-		LLAvatarIconCtrl::Params icon_params;
-		icon_params.avatar_id = session_id.notNull() ? LLIMModel::getInstance()->getOtherParticipantID(session_id) : LLUUID();
-		icon = LLUICtrlFactory::instance().create<LLAvatarIconCtrl>(icon_params);
-	}
-	mTabContainer->setTabImage(floaterp, icon);
-
-	mSessions[session_id] = floaterp;
-	floaterp->mCloseSignal.connect(boost::bind(&LLFloaterIMContainerBase::onCloseFloater, this, session_id));
-}
-
-void LLFloaterIMContainerBase::onCloseFloater(const LLUUID& session_id)
-{
-	mSessions.erase(session_id);
-	setFocus(TRUE);
-}
-
-// static
-LLFloaterIMContainerBase* LLFloaterIMContainerBase::findInstance()
-{
-	return LLFloaterReg::findTypedInstance<LLFloaterIMContainerBase>("im_container");
-}
-
-// static
-LLFloaterIMContainerBase* LLFloaterIMContainerBase::getInstance()
-{
-	return LLFloaterReg::getTypedInstance<LLFloaterIMContainerBase>("im_container");
-}
-
-// static
-LLFloater* LLFloaterIMContainerBase::buildFloater(const LLSD& sdKey)
-{
-	LLFloater* pIMContainer = NULL;
-	if (gSavedSettings.getBOOL("IMUseTabbedContainer"))
-		pIMContainer = new LLFloaterIMContainerTab(sdKey);
-	else
-		pIMContainer = new LLFloaterIMContainerView(sdKey);
-	return pIMContainer;
-}
-
-// static
-const std::string& LLFloaterIMContainerBase::getFloaterXMLFile()
-{
-	static std::string strFile;
-	strFile = 
-		(gSavedSettings.getBOOL("IMUseTabbedContainer")) 
-			? (!gSavedSettings.getBOOL("IMUseVerticalTabs")) ? "floater_im_container_tab_horiz.xml" : "floater_im_container_tab_vert.xml"
-			: "floater_im_container.xml";
-	return strFile;
-}
-
-void LLFloaterIMContainerBase::setMinimized(BOOL b)
-{
-	bool was_minimized = isMinimized();
-	LLMultiFloater::setMinimized(b);
-
-	// Switching from minimized to un-minimized
-	if (was_minimized && !b)
-	{
-		const LLUUID& session_id = getSelectedSession();
-		LLFloaterIMSessionTab* session_floater = LLFloaterIMSessionTab::findConversation(session_id);
-
-		if (session_floater && !session_floater->isTornOff())
-		{
-			// When in DND mode, remove stored IM notifications
-			// Nearby chat (Null) IMs are not stored while in DND mode, so can ignore removal
-			if (gAgent.isDoNotDisturb() && session_id.notNull())
-			{
-				LLDoNotDisturbNotificationStorage::getInstance()->removeNotification(LLDoNotDisturbNotificationStorage::toastName, session_id);
-			}
-		}
-	}
-}
-
-// static
-bool LLFloaterIMContainerBase::isConversationLoggingAllowed()
-{
-	return gSavedPerAccountSettings.getS32("KeepConversationLogTranscripts") > 0;
 }
 
 // EOF
