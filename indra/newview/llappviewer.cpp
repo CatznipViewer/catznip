0e1cc5148120e27c90451b06fb59f23f2307247d
Kitty Barnett <develop@catznip.com>
2014-02-08 11:48:09 +0100

    - fixed : merge error, don't send logout stats to LL twice
    --HG--
    branch : Viewer-Branding

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -4223,11 +4223,11 @@ void LLAppViewer::requestQuit()
 		gAgentAvatarp->updateAvatarRezMetrics(true); // force a last packet to be sent.
 	}
 	
-	// Try to send last batch of avatar rez metrics.
-	if (!gDisconnected && isAgentAvatarValid())
-	{
-		gAgentAvatarp->updateAvatarRezMetrics(true); // force a last packet to be sent.
-	}
+//	// Try to send last batch of avatar rez metrics.
+//	if (!gDisconnected && isAgentAvatarValid())
+//	{
+//		gAgentAvatarp->updateAvatarRezMetrics(true); // force a last packet to be sent.
+//	}
 	
 	LLHUDEffectSpiral *effectp = (LLHUDEffectSpiral*)LLHUDManager::getInstance()->createViewerEffect(LLHUDObject::LL_HUD_EFFECT_POINT, TRUE);
 	effectp->setPositionGlobal(gAgent.getPositionGlobal());
