30bb63720c5c3c2d628ab61c8fa3c0e2f7f63166
Kitty Barnett <develop@catznip.com>
2014-07-02 14:47:15 +0200

    - fixed : "Add datestamp" in the Chat / Logging preferences panel is always disabled
    --HG--
    branch : Chat-Logs

--- indra/newview/llfloaterpreference.cpp
+++ indra/newview/llfloaterpreference.cpp
@@ -1631,6 +1631,9 @@ void LLFloaterPreference::setPersonalInfo(const std::string& visibility, bool im
 	getChildView("log_nearby_chat")->setEnabled(TRUE);
 	getChildView("log_instant_messages")->setEnabled(TRUE);
 // [/SL:KB]
+// [SL:KB] - Patch: Preferences-Chat | Checked: 2014-07-02 (Catznip-3.6)
+	getChildView("logfile_name_datestamp")->setEnabled(TRUE);
+// [/SL:KB]
 //	getChildView("chat_font_size")->setEnabled(TRUE);
 }
 
