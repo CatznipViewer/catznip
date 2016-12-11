3881a8919713c71f51f1b1d370f52b31e6db358f
Kitty Barnett <develop@catznip.com>
2016-12-11 19:11:50 +0100

    [FIXED] CRASH-21: LLPanelWearing::hasItemSelected()
    --HG--
    branch : Appearance-Wearing

--- indra/newview/llpanelwearing.cpp
+++ indra/newview/llpanelwearing.cpp
@@ -1043,7 +1043,7 @@ bool LLPanelWearing::hasItemSelected()
 	{
 		return mCOFItemsList->getSelectedItem() != NULL;
 	}
-	else if (mInvPanel->getVisible())
+	else if ( (mInvPanel) && (mInvPanel->getVisible()) )
 	{
 		return mInvPanel->getRootFolder()->getCurSelectedItem() != NULL;
 	}
