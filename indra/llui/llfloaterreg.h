07b1575453f1aec8461ad76ecc0ba55c3cd8d6aa
Kitty Barnett <develop@catznip.com>
2017-04-22 15:15:47 +0200

    Reviewed for Catznip-5.2
    --HG--
    branch : UI-Base

--- indra/llui/llfloaterreg.h
+++ indra/llui/llfloaterreg.h
@@ -42,7 +42,7 @@ class LLFloater;
 class LLUICtrl;
 
 typedef boost::function<LLFloater* (const LLSD& key)> LLFloaterBuildFunc;
-// [SL:KB] - Patch: UI-Base | Checked: 2010-12-01 (Catznip-2.4)
+// [SL:KB] - Patch: UI-Base | Checked: Catznip-2.4
 typedef boost::function<const std::string& (void)> LLFloaterFileFunc;
 // [/SL:KB]
 
@@ -59,7 +59,7 @@ public:
 	struct BuildData
 	{
 		LLFloaterBuildFunc mFunc;
-// [SL:KB] - Patch: UI-Base | Checked: 2010-12-01 (Catznip-2.4)
+// [SL:KB] - Patch: UI-Base | Checked: Catznip-2.4
 		LLFloaterFileFunc mFileFunc;
 // [/SL:KB]
 		std::string mFile;
@@ -92,9 +92,9 @@ public:
 	static void add(const std::string& name, const std::string& file, const LLFloaterBuildFunc& func,
 					const std::string& groupname = LLStringUtil::null);
 
-// [SL:KB] - Patch: UI-Base | Checked: 2010-12-01 (Catznip-2.4)
+// [SL:KB] - Patch: UI-Base | Checked: Catznip-2.4
 	static void addWithFileCallback(const std::string& name, const LLFloaterFileFunc& fileFunc, const LLFloaterBuildFunc& func,
-					const std::string& groupname = LLStringUtil::null);
+	                                const std::string& groupname = LLStringUtil::null);
 // [/SL:KB]
 
 	// Helpers
