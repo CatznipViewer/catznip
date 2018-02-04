df149db77c8a61329f5dfb095e14998c2f8e35c4
Kitty Barnett <develop@catznip.com>
2018-02-04 15:59:49 +0100

    [FIXED] Crash reporter doesn't understand status 400 code(s)   -> Retrying the request isn't likely to result in a success when the problem is on the client's end so don't keep spamming the crash report server
    --HG--
    branch : Viewer-CrashReporting

--- indra/llcrashlogger/llcrashlogger.cpp
+++ indra/llcrashlogger/llcrashlogger.cpp
@@ -137,6 +137,12 @@ void LLCrashLoggerHandler::onFailure(LLCore::HttpResponse * response, LLCore::Ht
 {
     LL_WARNS("CRASHREPORT") << "Request to " << response->getRequestURL()
                             << " failed: " << status.toString() << LL_ENDL;
+// [SL:KB] - Patch: Viewer-CrashReporting | Checked: Catznip-5.3
+	if ( (status.getStatus() >= 400) && (status.getStatus() <= 499) )
+	{
+		gSent = true;
+	}
+// [/SL:KB]
     gBreak = true;
 }
 
