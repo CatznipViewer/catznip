58f5de1049af21a5589d4d481434c48b380d2f38
Kitty Barnett <develop@catznip.com>
2020-03-08 18:04:01 +0100

    CATZ-552: Mini-map shows X's rather than chevrons for nearby people >1020m up

--- indra/newview/llviewerregion.cpp
+++ indra/newview/llviewerregion.cpp
@@ -2149,6 +2149,8 @@ void LLViewerRegion::updateCoarseLocations(LLMessageSystem* msg)
 				const LLViewerObject* pAvatarObj = gObjectList.findObject(agent_id);
 				if (pAvatarObj)
 					z_pos16 = pAvatarObj->getPositionRegion().mV[VZ] / 4;
+				else
+					z_pos16 = MAX_OBJECT_Z / 4;
 			}
 // [/SL:KB]
 		}
